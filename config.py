653baf5886f8554d65c4460067e74c3043608c87
Arilson Souza <arilsons61@gmail.com>
2020-07-25 19:49:46 -0300

    Chapter 7: Error Handling

--- config.py
+++ config.py
@@ -14,3 +14,10 @@ class Config(object):
 
 	SQLALCHEMY_DATABASE_URI = DB_URL or 'sqlite:///' + os.path.join(BASEDIR, 'app.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+	MAIL_SERVER = os.environ.get('MAIL_SERVER')
+	MAIL_PORT = os.environ.get('MAIL_PORT')
+	MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
+	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+	ADMINS = ['your-email@example.com']
