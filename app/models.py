a312c8550f0749687ed213aed7706453b1cfe3f1
Arilson Souza <arilsons61@gmail.com>
2020-07-25 18:25:02 -0300

    Chapter 5: User Logins

--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
 from datetime import datetime
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
-from app import db
+from app import db, login
 
-class User(db.Model):
+class User(UserMixin, db.Model):
 	__tablename__ = 'users'
 
 	id = db.Column(db.Integer, primary_key=True)
@@ -11,6 +13,12 @@ class User(db.Model):
 	password_hash = db.Column(db.String(128))
 	
 	posts = db.relationship('Post', backref='author', lazy='dynamic')
+	
+	def set_password(self, password):
+		self.password_hash = generate_password_hash(password)
+
+	def check_password(self, password):
+		return check_password_hash(self.password_hash, password)
 
 	def __repr__(self):
 		return f'<User {self.username}>'
@@ -23,3 +31,7 @@ class Post(db.Model):
 
 	def __repr__(self):
 		return f'<Post {self.body}>'
+
+@login.user_loader
+def load_user(id):
+	return User.query.get(int(id))
